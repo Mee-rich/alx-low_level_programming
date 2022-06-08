46769058796589d1fc53c6ea9a90d2d5d3cb099f
Meerich <jamessuyi@gmail.com>
2022-06-08 11:20:22 -0700

    c basics

---
+++ 5-printf.c
@@ -0,0 +1,22 @@
+#include<studio.h>
+
+/**
+ *
+ * main-Entry point
+ *
+ * Return: Always 0 (success)
+ * 
+ */
+ 
+int main(void)
+
+{
+
+		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
+
+}
+
---
+++ 6-size.c
@@ -0,0 +1,36 @@
+#include <studio.h>
+/**
+ *
+ *main-Entry point
+ *
+ * Return: Always return 0 (success
+ *
+*/
+	
+
+	 int main (void)
+		{
+			char acharacter;
+			int ainteger;
+			long along;
+			long long alonglong;
+			float afloat;
+
+			printf("
+					size of char:%lu byte(s)\n", sizeof 
+					(acharacter));
+
+			printf ("size of integer:%lu byte(s)\n", 
+				sizeof (ainteger))
+
+			printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+
+				printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+
+					printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
+
+
+
+						return (0);
+		
+		}
