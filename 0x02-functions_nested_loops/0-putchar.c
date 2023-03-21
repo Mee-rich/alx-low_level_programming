1e20cd5674f321768278d52ebdf7e28e5f8e23f3
mee-rich <jamessuyi@gmail.com>
2023-03-21 14:23:45 -0700

    nested functions

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include "_putchar"
+
+/**
+ * main - prints a word
+ *
+ * Description: just prints a statement
+ * Return: 0 On success
+ */
+
+
+int main(void)
+{
+	char word[] = "_putchar";
+	putchar("%s\n",word);
+	
+	return (0);
+}
