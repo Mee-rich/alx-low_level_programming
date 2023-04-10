3ba8a0ded0e7a07a3adab13542beb792f63d6900
mee-rich <jamessuyi@gmail.com>
2023-04-10 06:53:19 -0700

    update

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,42 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-
 /**
- * main - adds positive numbers
- *
- * @argc: argument counter
- *
- * @argv: array of arguments
- *
- * Retrun: Always 0 (success)
+* main - prints the number of arguments passed into it.
+* @argc: - # of parameters
+* @argv: - array of parameters
+* Return: Always 0.
 */
-
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    int i, digit, sum = 0;
-	
+	int sum = 0, n, i, j;
+
 	if (argc == 1)
 	{
 		printf("0\n");
 	return (0);
 	}
-
-    for(i = 1; i < argc; i++)
-    {
-
-        if(argv[i][digit]  > '0' || argv[i][digit] > '9' )
-        {
-            sum +=  atoi(argv[i]);
-        }
-        else
-        {
-            printf("Error\n");
-            return (1);
-        }
-
-    }
-    printf("%d\n", sum);
-            return (0);
+	for (i = 1; i < argc; i++)
+	{
+		if (*argv[i] == 48)
+		{
+		n = 0;
+		}
+		else
+		{
+			for (j = 0; argv[i][j] != '\0'; j++)
+			{
+				if (argv[i][j] > 57 || argv[i][j] < 48)
+				{  printf("Error\n");
+				return (1); }
+			}
+			if (atoi(argv[i]) == 0)
+			{ printf("Error\n");
+			return (1); }
+			n = atoi(argv[i]);
+			sum += n;
+		}
+	}
+	printf("%d\n", sum);
+return (0);
 }
 
