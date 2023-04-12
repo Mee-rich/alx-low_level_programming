70a26ab7874b419f8ccb2ec25d1778d7b769f8b3
mee-rich <jamessuyi@gmail.com>
2023-04-12 02:59:03 -0700

    args_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,20 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 
 /**
- * main - a program that renames itself
+ * main - Prints the program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
  *
- * it changes to the inputted name
- *
- * @argc - number of arguments
- *
- * @argv - array of arguments
- *
-*/
-
-int main(int __attribute__((unused)) argc, char* argv[])
+ * Return: Always o.
+ */
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-     printf ("%s\n", argv[0]);
-     return 0;
+	printf("%s\n", argv[0]);
+
+	return (0);
 }
