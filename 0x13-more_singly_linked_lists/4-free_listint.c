1c8ce704857a0908d07dcdb6754b100376df7f61
mee-rich <jamessuyi@gmail.com>
2023-05-01 03:22:20 -0700

    update

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,20 +3,20 @@
 /**
  * free_listint - fuction that frees a listint_t list
  * @head: pointer to listint_t list
- * 
+ *
  * Return: Free the listint_t list on success
 */
 
 void free_listint(listint_t *head)
 {
-    listint_t *temp = head;
-    listint_t *traverse;
+	listint_t *temp = head;
+	listint_t *traverse;
 
-    while (temp != NULL) /*check if the head is null*/
-    {
-        /*traverse the list and save head to temp*/
-        traverse = temp; 
-        temp = temp->next;
-        free(traverse); /*free each non empty node*/
-    }
+	while (temp != NULL) /*check if the head is not null*/
+	{
+		/*traverse the list and save head to temp*/
+		traverse = temp;
+		temp = temp->next;
+		free(traverse); /*free each non empty node*/
+	}
 }
