66a4af9cd383a7d62238b7ca1f6194430779cb84
mee-rich <jamessuyi@gmail.com>
2023-05-02 12:49:29 -0700

    update

--- 0x13-more_singly_linked_lists/main_test/0-print_listint.c
+++ 0x13-more_singly_linked_lists/main_test/0-print_listint.c
@@ -3,14 +3,14 @@
 /**
  * size_t print_listint - prints all the elements of a linked list
  * @h: pointer to the list
- * 
+ *
  * Return:  the number of nodes
 */
 
 size_t print_listint(const listint_t *h)
 {
 	size_t count = 0;
-	
+
 	while (h)
 	{
 		count++;
