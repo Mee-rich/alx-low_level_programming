f3214592e76ee99d366b3e5b8a42983b0f33eddb
Mee-rich <jamessuyi@gmail.com>
2023-11-29 21:21:18 -0800

    new changes

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,19 +1,4 @@
 #include "main.h"
-#include <stddef.h>
-/**
- * @str_len - string length counter
- * @str - pointer to string of file
- *
- * Return: string length 
- */
-int str_len(char *str)
-{
-	int len = 0;
-	while (str[len] != '\0')
-		len++;
-	return (len);
-}
-
 /**
  * append_text_to _file - Function that appends 
  * 	text at the end of a file.
@@ -29,27 +14,25 @@ int str_len(char *str)
  * 	-1 if the file does not exist  or not 
  * 	permitted to write to file.
  */
-int append_text_to _text_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
-	int o_file, w_file;
+	int o_file, w_file, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 	
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len]; )
+			len++;
+	}
+	
 	o_file = open(filename, O_WRONLY | O_APPEND);
-	if (o_file != NULL)
-	{
-		w_file = write(o_file, text_content, str_len(text_content));
-		if (w_file == -1)
-		{
-			close(file);
-			return(-1);
-		}
-		close(file);
-		return (1);
-	}
-	else
-	{
-		close(file);
-		return (1);
-	}
+	w_file = write(o_file, text_content, len);
+
+	if (o_file == -1 || w_file == -1)
+		return (-1);
+	
+	close (o_file);
+	return (1);
+}
