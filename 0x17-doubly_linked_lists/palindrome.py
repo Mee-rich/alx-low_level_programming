321d019735a34bd94cfc74f9f61c6ce355b91382
mee-rich <jamessuyi@gmail.com>
2024-01-15 16:31:45 +0100

    Dynamic Libraries

---
+++ 0x17-doubly_linked_lists/palindrome.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def is_palindrome(num):
+    return str(num) == str(num)[::-1]
+
+def largest_palindrome():
+    max_palindrome = 0
+    for i in range(100, 1000):
+        for j in range(100, 1000):
+            product = i * j
+            if is_palindrome(product) and product > max_palindrome:
+                max_palindrome = product
+    return (max_palindrome)
+print (largest_palindrome())
