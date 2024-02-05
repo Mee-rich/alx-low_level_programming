37c0d57c2e8c0bc7a9b5c4a14b62ad2039cac783
mee-rich <jamessuyi@gmail.com>
2024-02-05 22:21:24 +0100

    ok

--- 0x0A-python-inheritance/1-my_list.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-"""MyList module"""
-
-class MyList(list):
-    """MyList class - Inherits from list"""
-    print(sorted(self))
--- 0x0A-python-inheritance/2-is_same_class.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-"""Module containing is_same_class_method"""
-
-def is_same_class(obj, a_class):
-    """Returns:
-    True: if the object is exactly an instance of the specified  class
-    False: otherwise
-    """
-    return isinstance(obj, a_class) and type(obj) is a_class
